538ed414bc44ca8354c1bfbb583c5ad22eba7d2b
dronz <josh.dronsfield@gmail.com>
2020-07-04 14:52:21 +0100

    adding an svg and a rect and using interactjs

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css"
+import interact from "interactjs"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const SVG_NS = "http://www.w3.org/2000/svg"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function setAttrs(
+  el: HTMLElement | SVGGraphicsElement | SVGRectElement,
+  attrs: { [name: string]: string }
+) {
+  Object.entries(attrs).forEach(([key, value]) => {
+    el.setAttribute(key, value)
+  })
+}
+
+const svg = document.createElementNS(SVG_NS, "svg")
+svg.id = "the-svg"
+document.body.appendChild(svg)
+
+const tile = document.createElementNS(SVG_NS, "rect")
+setAttrs(tile, {
+  class: "tile",
+  height: "100",
+  width: "70",
+  fill: "red"
+})
+svg.appendChild(tile)
+
+interact(".tile").draggable({
+  listeners: {
+    start: (event) => {
+      console.log("START", { event })
+    },
+    move: (event) => {
+      console.log("MOVE", { event })
+      setAttrs(event.target, {
+        transform: `translate(${event.page.x - event.x0}, ${
+          event.page.y - event.y0
+        })`
+      })
+    }
+  }
+})
